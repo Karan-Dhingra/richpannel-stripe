24cb72bf615647dd0f5a5db3f328aa1bef0b19ce
karan-dhingra <dhingrakaran764@gmail.com>
2023-08-11 02:18:09 +0530

    backend-url

--- src/constant.js
+++ src/constant.js
@@ -1,4 +1,5 @@
-export const BACKEND_URL = 'http://localhost:5010'
+// export const BACKEND_URL = 'http://localhost:5010'
+export const BACKEND_URL = 'https://long-pink-crayfish-gear.cyclic.app'
 export const SECRET_CODE = 'THIS IS A SECRET DONT TRY TO COPY'
 
 export const ErrorMessage = (error) => {
